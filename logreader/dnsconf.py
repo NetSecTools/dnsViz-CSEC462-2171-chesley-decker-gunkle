1e57d6412e00b7312feb29f2cd3bcf150b19022d
Liam <liam@jane.wireless.rit.edu>
2017-11-03 17:03:58 -0400

    finished file reading, parsing

--- logreader/dnsconf.py
+++ logreader/dnsconf.py
@@ -1,9 +1,10 @@
 QUERY_LOG_FILE = "./thing"
-SLEEP_TIMER = 10
+SLEEP_TIMER = 1
 LOG_REGEX = "(\S{1,3}) (\d{1,2}) (\d{1,2}:\d{1,2}:\d{1,2}) .*: client (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#\d{1,5}):" \
             " query: (.*) IN (.*) (\+|\-)(\S{0,3})"#Logs are long
 """
-REGEX FORMAT
+REGEX FORMAT:
+
 MONTH
 DATE
 TIME (24 hour, hh:mm:ss)
@@ -14,16 +15,16 @@ RECURSION (+/- for yes/no)
 FLAGS
 """
 BIND_MONTHS_DICT = {
-    "Jan" : 1,
-    "Feb" : 2,
-    "Mar" : 3,
-    "Apr" : 4,
-    "May" : 5,
-    "Jun" : 6,
-    "Jul" : 7,
-    "Aug" : 8,
-    "Sep" : 9,
-    "Oct" : 10,
-    "Nov" : 11,
-    "Dec" : 12
+    "jan" : 1,
+    "feb" : 2,
+    "mar" : 3,
+    "apr" : 4,
+    "may" : 5,
+    "jun" : 6,
+    "jul" : 7,
+    "aug" : 8,
+    "sep" : 9,
+    "oct" : 10,
+    "nov" : 11,
+    "dec" : 12
 }
--- logreader/main.py
+++ logreader/main.py
@@ -6,6 +6,7 @@ from time import sleep
 from dnsconf import QUERY_LOG_FILE as LOGGING_FILE
 from dnsconf import SLEEP_TIMER
 from dnsconf import LOG_REGEX
+from dnsconf import BIND_MONTHS_DICT
 
 """Known constraints: This WILL NOT work for very large files
    I'm also making some guesses about the log file and what's imortant"""
@@ -15,18 +16,20 @@ Returns: An array of arrays of parsed data
 Destroys the file to save space because query logs can blow up quickly
 """
 def parseFile(fileName):
-    lines = ""
+    lines = [] #Lines of the log file
     with open(LOGGING_FILE, "r+") as logFile:
         lines = logFile.readlines()
-        logFile.truncate()
+        logFile.truncate(0)
     regexExp = re.compile(LOG_REGEX)
-    lines = lines.split["\n"]
     parsedOutput = []
     for line in lines:
-       matchObj = re.match(line)
-       parsedLine = []
-       for i in range(0, 7):
-          parsedLine.append(matchObj.group(i))
+        matchObj = regexExp.match(line)
+        parsedLine = []
+        for i in range(1, 9):
+            parsedLine.append(matchObj.group(i))
+        parsedOutput.append(parsedLine)
+    for line in parsedOutput:
+        print(line)
 
 def main():
     while(True):
