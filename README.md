8d8f4d8b574996e87005f98adf4ad2c1b127b5e8
Caleb <caleb.chesley@gmail.com>
2017-11-25 21:25:08 -0500

    Totally revamped file reading and updated README.md with github style formatting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-DNSMap
+#DNSMap
 
 DNS Visualization Tool designed to provide a representation of DNS traffic on a network.
 Information about records accessed will be presented, covering records handed out by the server.
@@ -7,39 +7,39 @@ Information about requests and reponses outside of the host network will be pres
 giving a geographic representation of the location of addresses being resolved by the tool.
 
 
+##Authors
+Caleb Chesley - csc1918@rit.edu - Visualization and data interpretation
+Liam Decker - wgd6034@rit.edu - DNS query collection and processing
+Andrew Gunkle - arg5173@rit.edu - DNSSec validation and repository maintenance
 
 
-Prerequisites
-
+##Prerequisites
 bind9
-
 Python 3.5 or higher
 
-Installation
 
-Logreader
+##Installation
+###Logreader
 To use the logreader to create csv's, change the file location in "logreader/dnsconf.py"
-
 If you would like to keep your query logs, make sure this is pointed at a copy of your logs because it will destroy the logs in order to keep log size down
-
 Run main.py with python 3.5 or higher, it will continuously read from the designated log and check in after the SLEEP\_TIMER interval
-
 LOCATION\_URL is the designated api to reach out to for location data. If desired, this can be changed, but the findLocation() function may need to be updated to the requirements of that api
-
-Visualizer
-
+###Visualizer
 The visualizer requires an initial internet connection for use unless manually reconfigured with local resources.
-"index.html" includes a "// config variables //" field, which dictates the Latitude and Longitude of the DNS server,
+"index.html" includes a **"// config variables //"** field, which dictates the Latitude and Longitude of the DNS server,
 and the source CSV to retrieve queries from.
+Config variables:
+    sourceFile: Path to the CSV file used for query input
+    DNSServerLat: Geographic latitude for the DNS server hosting the logreader
+    DNSServerLong: Geographic longitude for the DNS server hosting the logreader
 CSV Format:
 Day,Month,Year,Timestamp,SourceIP#SourcePort,query,Record Type,+,E,region,Region Name,Country,Country Code, Lat,Lon
 
 
-Built With
-
-Built With (Resource - Website - Use):
-IP API - ip-api.com - Location resolution for IP addresses
-D3.js - d3js.org - Visualizer construction
+##Resources
+###Built With (Resource - Website - Use):
+[IP API](ip-api.com) - Location resolution for IP addresses
+[D3.js](d3js.org - Visualizer construction
 Leaflet - leafletjs.com - Map function and geographic data visualization
 Canvasjs - canvasjs.com - Statistics and charts
 Mapbox - mapbox.com - Map tiles for Leaflet
@@ -48,15 +48,6 @@ Mapbox - mapbox.com - Map tiles for Leaflet
     Enter the site and create an account for a limited key.
 
 
-Authors
-
-Caleb Chesley - csc1918@rit.edu
-
-Liam Decker - wgd6034@rit.edu
-
-Andrew Gunkle - arg5173@rit.edu
-
-
-License
+##License
 
 This project is licensed under the Apache License, Version 2.0
--- visualizer/src/createChart.js
+++ visualizer/src/createChart.js
@@ -2,112 +2,103 @@
 
 
 
-function createQueryChart(sourceFile) {
-    d3.text(sourceFile, function(content) {
-
-        var data = d3.csvParseRows(content);
-
-        var queryCount = {};
-        var query;
-        var row;
-        for (var i = 0; i < data.length; i++) {
-            row = data[i];
-            query = row[5];
-            if (queryCount.hasOwnProperty(query)) {
-                queryCount[query]++;
-            }
-            else {
-                queryCount[query] = 1;
-            }
+function createQueryChart(data) {
+    var queryCount = {};
+    var query;
+    var row;
+    for (var i = 0; i < data.length; i++) {
+        row = data[i];
+        query = row[5];
+        if (queryCount.hasOwnProperty(query)) {
+            queryCount[query]++;
         }
-
-        var querySorted = [];
-        for (var q in queryCount) {
-            querySorted.push([q, queryCount[q]]);
+        else {
+            queryCount[query] = 1;
         }
+    }
 
-        querySorted.sort(function(a, b) {
-            return b[1] - a[1];
-        });
+    var querySorted = [];
+    for (var q in queryCount) {
+        querySorted.push([q, queryCount[q]]);
+    }
 
-
-        var queryData = querySorted;
-        var queryDataPoints = [];
-        for (i = 0; i < queryData.length; i++) {
-            queryDataPoints.push({label: queryData[i][0], y: queryData[i][1]});
-
-        }
-        var queryChart = new CanvasJS.Chart("queryChartContainer", {
-            animationEnabled: true,
-
-            title: {
-                text: "Top 10 queries by frequency of query"
-            },
-            data: [{
-                type: "column", //change type to bar, line, area, pie, etc
-                indexLabel: "{y}", //Shows y value on all Data Points
-                indexLabelFontColor: "#5A5757",
-                indexLabelPlacement: "outside",
-                dataPoints: queryDataPoints
-            }]
-        });
-        queryChart.render();
+    querySorted.sort(function(a, b) {
+        return b[1] - a[1];
     });
+
+
+    var queryData = querySorted;
+    var queryDataPoints = [];
+    for (i = 0; i < queryData.length; i++) {
+        queryDataPoints.push({label: queryData[i][0], y: queryData[i][1]});
+
+    }
+    var queryChart = new CanvasJS.Chart("queryChartContainer", {
+        animationEnabled: true,
+
+        title: {
+            text: "Top 10 queries by frequency of query"
+        },
+        data: [{
+            type: "column", //change type to bar, line, area, pie, etc
+            indexLabel: "{y}", //Shows y value on all Data Points
+            indexLabelFontColor: "#5A5757",
+            indexLabelPlacement: "outside",
+            dataPoints: queryDataPoints
+        }]
+    });
+    queryChart.render();
 }
 
 
 
 
-function createSourceChart(sourceFile) {
-    d3.text(sourceFile, function(content) {
-        var data = d3.csvParseRows(content);
-
-        var queryCount = {};
-        var srcIP;
-        var srcAndPort;
-        var row;
-        for (var i = 0; i < data.length; i++) {
-            row = data[i];
-            srcAndPort = row[4];
-            srcAndPort = srcAndPort.split("#");
-            srcIP = srcAndPort[0];
-            if (queryCount.hasOwnProperty(srcIP)) {
-                queryCount[srcIP]++;
-            }
-            else {
-                queryCount[srcIP] = 1;
-            }
+function createSourceChart(data) {
+    var queryCount = {};
+    var srcIP;
+    var srcAndPort;
+    var row;
+    for (var i = 0; i < data.length; i++) {
+        row = data[i];
+        srcAndPort = row[4];
+        srcAndPort = srcAndPort.split("#");
+        srcIP = srcAndPort[0];
+        if (queryCount.hasOwnProperty(srcIP)) {
+            queryCount[srcIP]++;
         }
-        var querySorted = [];
-        for (var q in queryCount) {
-            querySorted.push([q, queryCount[q]]);
+        else {
+            queryCount[srcIP] = 1;
         }
+    }
+    var querySorted = [];
+    for (var q in queryCount) {
+        querySorted.push([q, queryCount[q]]);
+    }
 
-        querySorted.sort(function(a, b) {
-            return b[1] - a[1];
-        });
-
-
-        var queryData = querySorted;
-        var sourceDataPoints = [];
-        for (i = 0; i < queryData.length; i++) {
-            sourceDataPoints.push({label: queryData[i][0], y: queryData[i][1]});
-            //console.log(queryData[i]);
-        }
-        var sourceChart = new CanvasJS.Chart("sourceChartContainer", {
-            animationEnabled: true,
-
-            title: {
-                text: "Top 10 sources by frequency of request"
-            },
-            data: [{
-                type: "column", //change type to bar, line, area, pie, etc
-                indexLabel: "{y}", //Shows y value on all Data Points
-                indexLabelFontColor: "#5A5757",
-                indexLabelPlacement: "outside",
-                dataPoints: sourceDataPoints
-            }]
-        });
-        sourceChart.render();
+    querySorted.sort(function(a, b) {
+        return b[1] - a[1];
     });
+
+
+    var queryData = querySorted;
+    var sourceDataPoints = [];
+    for (i = 0; i < queryData.length; i++) {
+        sourceDataPoints.push({label: queryData[i][0], y: queryData[i][1]});
+        //console.log(queryData[i]);
+    }
+    var sourceChart = new CanvasJS.Chart("sourceChartContainer", {
+        animationEnabled: true,
+
+        title: {
+            text: "Top 10 sources by frequency of request"
+        },
+        data: [{
+            type: "column", //change type to bar, line, area, pie, etc
+            indexLabel: "{y}", //Shows y value on all Data Points
+            indexLabelFontColor: "#5A5757",
+            indexLabelPlacement: "outside",
+            dataPoints: sourceDataPoints
+        }]
+    });
+    sourceChart.render();
 }
